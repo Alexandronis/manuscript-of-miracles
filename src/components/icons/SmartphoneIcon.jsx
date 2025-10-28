587e90ea6f267d9c7459b42ca1e31e802dfaae1b
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-10-28 18:43:01 +0200

    fixed icons for main content tab.

--- src/components/icons/SmartphoneIcon.jsx
+++ src/components/icons/SmartphoneIcon.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 const SmartphoneIcon = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    width="24" height="24"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
+    stroke="#000000"
     strokeWidth={2}
     strokeLinecap="round"
     strokeLinejoin="round"
+    className="lucide lucide-smartphone w-5 h-5"
     {...props}
   >
-    <rect x={7} y={2} width={10} height={20} rx={2} ry={2} />
-    <line x1={12} y1={18} x2={12.01} y2={18} />
+    <rect width="14" height="20" x="5" y="2" rx="2" ry="2"></rect>
+    <path d="M12 18h.01"></path>
   </svg>
 );
 
